0db9abd6e858dd71cb88b6ca31ad3300b79be981
Michael McGovern <mmcgovern.wework@gmail.com>
2019-04-12 01:31:34 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -9,7 +9,11 @@ def summon_captain_planet(planeteer_calls)
 end
 
 def long_planeteer_calls(planeteer_calls)
-  
+  planeteer_calls.each do |call|
+    if call.length >= 4
+      true
+    end
+    false
 end
 
 def find_the_cheese# code an argument here
